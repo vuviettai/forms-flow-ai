2f49c1b33a0e94598185b7f85d0ed1123e17b187
Vu Viet Tai <vuviettai@gmail.com>
2023-09-19 21:54:45 +0700

    Update running script for macos and linux

--- deployment/docker/keycloak.sh
+++ deployment/docker/keycloak.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 ENV_FILE=$DIR/formsflow.env
-# For linux
-HOST_IP=$(hostname -I | awk '{print $1}')
-sed -i "s|HOST_IP=.*|HOST_IP=$HOST_IP|g" $ENV_FILE
-# Form macos
-# HOST_IP=$(ipconfig getifaddr en0)
-# echo 'Start forms flow with ip ${HOST_IP}'
-# sed -i '' "s|HOST_IP=.*|HOST_IP=$HOST_IP|g" $ENV_FILE
+OS=$(uname)
+if [ $OS == 'Darwin' ]
+then
+    # Form macos
+    HOST_IP=$(ipconfig getifaddr en0)
+    sed -i '' "s|HOST_IP=.*|HOST_IP=$HOST_IP|g" $ENV_FILE
+elif [ $OS == 'Linux' ]
+then
+    HOST_IP=$(hostname -I | awk '{print $1}')
+    sed -i "s|HOST_IP=.*|HOST_IP=$HOST_IP|g" $ENV_FILE
+fi 
 COMPOSES="-f ${DIR}/docker-compose-keycloak.yml"
 build() {
     docker-compose ${COMPOSES} \
