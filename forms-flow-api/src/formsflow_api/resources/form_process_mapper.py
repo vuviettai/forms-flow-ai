70b687718ae84f6fe24ab39a6349158da06cd6ee
Vu Viet Tai <vuviettai@gmail.com>
2023-11-05 22:30:45 +0700

    Revert form api

--- forms-flow-api/src/formsflow_api/resources/form_process_mapper.py
+++ forms-flow-api/src/formsflow_api/resources/form_process_mapper.py
@@ -216,8 +216,7 @@ class FormResourceList(Resource):
             limit: int = dict_data.get("limit")
             sort_by: str = dict_data.get("sort_by", "id")
             sort_order: str = dict_data.get("sort_order", "desc")
-            #form_type: str = dict_data.get("form_type", "form")
-            form_type: str = dict_data.get("form_type")
+            form_type: str = dict_data.get("form_type", "form")
             is_active = dict_data.get("is_active", None)
 
             if form_name:
